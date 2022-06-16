d99d397b728a8bfcccb3791649859be6804c007f
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-16 20:55:51 +0530

    UseEffect End 1 file modified

--- src/components/Useeffect.js
+++ src/components/Useeffect.js
@@ -2,8 +2,28 @@ import React, { useState, useEffect } from 'react';
 export default function useeffect() {
   const [resolve, setResolve] = useState('posts');
   const [data, setData] = useState([]);
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log(width);
   console.log('render before ');
-
+  //Concept of useEffect hook
+  // useEffect(() => {
+  //   console.log('ComponentDidMount Method');
+  //   return () => {
+  //     console.log('ComponentWillUnmount Method');
+  //   };
+  // }, [width]);
+  function handleResize() {
+    setWidth(window.innerWidth);
+  }
+  // ComponentDidMount and ComponentWillUnmount
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    console.log('ComponentDidMount Method Final render');
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      console.log('ComponentWillUnmount Method');
+    };
+  }, [width]);
   // Call DummyApi
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resolve}`)
@@ -13,6 +33,8 @@ export default function useeffect() {
   }, [resolve]);
   return (
     <div>
+      <pre>{width}</pre>
+      <br />
       <button type="button" onClick={() => setResolve('posts')}>
         Posts
       </button>
@@ -30,7 +52,7 @@ export default function useeffect() {
       <div>
         {data.map((item) => {
           return (
-            <p>
+            <p key={item.id}>
               <b>Title:</b>
               {item.title} <b>Name:</b>
               {item.name}
