9b53999b326c84be38373b7fd89ad3c4080084d6
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-16 17:29:43 +0530

    ComponentDidMount and ComponentDidUpdate 1 file modified

--- src/components/Useeffect.js
+++ src/components/Useeffect.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 export default function useeffect() {
   const [resolve, setResolve] = useState('Posts');
-  console.log('render useEffect component ');
+  console.log('render before ');
   useEffect(() => {
-    console.log('ComponentDidMount Method');
-  });
+    console.log('ComponentDidMount Method Just time render');
+  }, []);
   //Just one time it's running on onClick method. because dependency in state
   useEffect(() => {
     console.log('ComponentDidUpdate Method');
   }, [resolve]);
+  // Call DummyApi
+ 
   return (
     <div>
-      <button type="button" onClick={() => setResolve('Posts')}>
+      <button type="button" onClick={() => setResolve('posts')}>
         Posts
       </button>
       <br />
-      <button type="button" onClick={() => setResolve('Users')}>
+      <button type="button" onClick={() => setResolve('users')}>
         Users
       </button>
       <br />
-      <button type="button" onClick={() => setResolve('Comments')}>
+      <button type="button" onClick={() => setResolve('comments')}>
         Comments
       </button>
       <br />
