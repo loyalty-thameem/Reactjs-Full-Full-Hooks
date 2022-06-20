82de9755abe92bf84792440237d19632ba964971
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-20 16:26:36 +0530

    UseReducer Basic step => 1 file modified

--- src/components/Usereducer.js
+++ src/components/Usereducer.js
@@ -1,23 +1,43 @@
 import React from 'react';
 export default function usereducer() {
   //useReducer similar to useState. but useReduce is bigdeal and complex. It's also managed state.
-  // const [state, dispatch] = React.useReducer(reducer, { count: 0 });
-  const [count, setCount] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, { count: 0 });
+  // const [count, setCount] = React.useState(0);
   //function of usereducer
-  // function reducer(){}
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'Increment':
+        return {
+          count: state.count + 1,
+        };
+      case 'Decrement':
+        return {
+          count: state.count + 1,
+        };
+    }
+    return {
+      count: state.count + 1,
+    };
+  }
   //Increment
   function handleClickIncrement() {
-    setCount((prevCount) => prevCount + 1);
+    // setCount((prevCount) => prevCount + 1);
+    dispatch({
+      type: 'Increment',
+    });
   }
   function handleClickdecrement() {
-    setCount((prevCount) => prevCount - 1);
+    // setCount((prevCount) => prevCount - 1);
+    dispatch({
+      type: 'Decrement',
+    });
   }
   return (
     <div>
       <h2>useReducer</h2>
       <span>
-        <b>{count}</b>
-        {/* <b>{state.count}</b> */}
+        {/* <b>{count}</b> */}
+        <b>{state.count}</b>
       </span>
       <br />
       <button type="button" onClick={handleClickIncrement}>
