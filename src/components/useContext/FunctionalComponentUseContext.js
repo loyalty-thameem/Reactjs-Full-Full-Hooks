95f410bfb42b499bfeb0f1347ebff56da11138ae
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-22 00:19:55 +0530

    1 file modified

--- src/components/useContext/FunctionalComponentUseContext.js
+++ src/components/useContext/FunctionalComponentUseContext.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { ThemesProvide } from './Usecontext.js';
 export default function Functionalcomponentusecontext() {
+  //FunctionalComponentUseContext and created it.
+  const darkTheme = React.useContext(ThemesProvide);
+  console.log('The theme from useContext and functional component');
+  const themeStyles = {
+    backgroundColor: darkTheme ? 'red' : 'yellow',
+    color: darkTheme ? 'blue' : 'black',
+    width: '250px',
+    height: '100px',
+  };
   return (
-    <div>
-      <h2>function</h2>
+    <div style={themeStyles}>
+      <h2>class</h2>
     </div>
   );
 }
